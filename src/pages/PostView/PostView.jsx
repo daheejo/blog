378acdaf7c32e17971df9bec2a388026756a707a
Dahee Jo <happyj6528@gmail.com>
2022-06-24 02:24:06 +0900

    Feat: 게시물 컴포넌트화

--- src/pages/PostView/PostView.jsx
+++ src/pages/PostView/PostView.jsx
@@ -1,2 +1,17 @@
 import './postView.css'
-import './wrapbox.css'
+
+import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
+import Footer from '../../components/Footer/Footer'
+import View from '../../components/View/View'
+
+export default function PostView(){
+    return (
+        <>
+			<Header/>
+            <Banner/>
+            <View/>
+            <Footer/>
+        </>
+    )
+}
