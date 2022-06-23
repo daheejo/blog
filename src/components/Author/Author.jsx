a8a2ee4d655048a67467cee111c99f4264398f44
Dahee Jo <happyj6528@gmail.com>
2022-06-23 21:21:39 +0900

    Chore: asset폴더를 public으로 복사

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -1,10 +1,10 @@
 import './author.css'
 
-export default function Author(){
+export default function Author(props){
     <dl className="author-wrap">
         <dt className="a11y-hidden">Author</dt>
-        <dd className="author"><img src={require("../../assets/images/profile.jpg")} alt="" /> Chilli</dd>
+        <dd className="author"><img src={require("../../assets/images/profile.jpg")} alt="" />{props.userName}</dd>
         <dt className="a11y-hidden">Created</dt>
-        <dd className="created">2022.05.25</dd>
+        <dd className="created">{props.created}</dd>
     </dl>
 }
