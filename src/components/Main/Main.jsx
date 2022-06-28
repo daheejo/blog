06aaed6ca978dcdc98a69ce37cf540aaad4a0995
Dahee Jo <happyj6528@gmail.com>
2022-06-28 21:36:19 +0900

    Fix: props 전달

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,12 +3,12 @@ import './main.css'
 import Posts from '../Posts/Posts'
 import About from '../About/About'
 
-export default function Main(){
+export default function Main(props){
     return(
     <main>
         <h2 class="a11y-hidden">Post</h2>
         <div class="max-width">
-        <Posts/>
+        <Posts posts={props.posts}/>
         <About/>
         </div>
     </main>
